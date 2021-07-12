4953184d97f7443f8293ab049581587e4afadb17
Bekir Çelik <bekir.celik@detaysoft.com>
2021-07-12 15:45:25 +0300

    Add project files.

---
+++ RSACryptography/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+using static RSACryptography.Program;
+
+namespace RSACryptography
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RsaEncryption rsa = new RsaEncryption();
+
+            var encryptText = "HelloWorld.123";
+
+            var cypher = rsa.Encrypt(encryptText);
+            var decryptText = rsa.Decrypt(cypher);
+
+            ConsoleWriteInColor("PlainText : ", encryptText, ConsoleColor.Red, ConsoleColor.Black);
+            ConsoleWriteInColor("Encrypt CypherText : " , cypher, ConsoleColor.Green, ConsoleColor.Black);
+            ConsoleWriteInColor("Decrypt PlainText : " , decryptText, ConsoleColor.Yellow, ConsoleColor.Black);
+
+            Console.ReadKey();
+        }
+
+        // Console renklendirmesi için yardımcı metod
+        static void ConsoleWriteInColor(string value, string value2, ConsoleColor ForegroundColor, ConsoleColor BackgroundColor, bool WriteLine = true, bool NullCheck = true)
+        {
+            if (NullCheck && string.IsNullOrWhiteSpace(value))
+                value = "-";
+            else
+            {
+                Console.BackgroundColor = BackgroundColor;
+                Console.ForegroundColor = ForegroundColor;
+            }
+
+            Console.Write(value);
+            Console.ResetColor();
+            Console.Write(value2);
+
+            if (WriteLine)
+                Console.WriteLine();
+        }
+    }
+}
---
+++ RSACryptography/RsaEncryption.cs
@@ -0,0 +1,87 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace RSACryptography
+{
+    public class RsaEncryption
+    {
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters privateKey;
+        private RSAParameters publicKey;
+
+        public RsaEncryption()
+        {
+
+            string public_pem = "C:\\Users\\Bekir\\Desktop\\openssl-0.9.8h-1-bin\\bin\\publicKey.pem";
+            string private_pem = "C:\\Users\\Bekir\\Desktop\\openssl-0.9.8h-1-bin\\bin\\privateKey.pem";
+
+            var pub = GetPublicKeyFromPemFile(public_pem);
+            var pri = GetPrivateKeyFromPemFile(private_pem);
+
+            publicKey = pub.ExportParameters(false);
+            privateKey = pri.ExportParameters(true);
+
+            // Automatic public-private key generator
+            /// privateKey = csp.ExportParameters(true);
+            /// publicKey = csp.ExportParameters(false);
+        }
+
+        public static RSACryptoServiceProvider GetPublicKeyFromPemFile(String filePath)
+        {
+            using (TextReader publicKeyTextReader = new StringReader(File.ReadAllText(filePath)))
+            {
+                RsaKeyParameters publicKeyParam = (RsaKeyParameters)new PemReader(publicKeyTextReader).ReadObject();
+
+                RSAParameters rsaParams = DotNetUtilities.ToRSAParameters((RsaKeyParameters)publicKeyParam);
+
+                RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+                csp.ImportParameters(rsaParams);
+                return csp;
+            }
+        }
+
+        public static RSACryptoServiceProvider GetPrivateKeyFromPemFile(string filePath)
+        {
+            using (TextReader privateKeyTextReader = new StringReader(File.ReadAllText(filePath)))
+            {
+                AsymmetricCipherKeyPair readKeyPair = (AsymmetricCipherKeyPair)new PemReader(privateKeyTextReader).ReadObject();
+
+                RSAParameters rsaParams = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)readKeyPair.Private);
+                RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+                csp.ImportParameters(rsaParams);
+                return csp;
+            }
+        }
+
+        public string Encrypt(string plainText)
+        {
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(publicKey);
+            var data = Encoding.Unicode.GetBytes(plainText);
+            var cypher = csp.Encrypt(data, false);
+            return Convert.ToBase64String(cypher);
+
+        }
+
+        public string Decrypt(string cypherText)
+        {
+            var dataBytes = Convert.FromBase64String(cypherText);
+            csp.ImportParameters(privateKey);
+            var plainText = csp.Decrypt(dataBytes, false);
+            return Encoding.Unicode.GetString(plainText);
+
+        }
+
+    }
+}
+
+
+
+
